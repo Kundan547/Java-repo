9682e43f6b45245e26d1e8232890eb23ec52fd4f
Kundan547 <Kundanvyas197@gmail.com>
2024-07-22 14:29:51 +0530

    "Anagram program by using java"

--- pro_1.java
+++ pro_1.java
@@ -1,5 +1,16 @@
-class pro_1{
+import java.util.Scanner;
+
+public class pro_1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        System.out.println("You entered: " + number);
+        
+        scanner.close();
     }
 }
